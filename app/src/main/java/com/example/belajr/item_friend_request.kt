f1c52573ed6f9ab443022dbf8e0521a3f69559eb
alipp <mhytxz705@gmail.com>
2026-04-04 21:39:57 +0700

    fix: force remove submodule app

---
+++ app/src/main/java/com/example/belajr/item_friend_request.kt
@@ -0,0 +1,20 @@
+package com.example.belajr
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class item_friend_request : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_item_friend_request)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
